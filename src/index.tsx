e65e7a84cc1d411df230d5f96fc4789ba8bbc446
leesangwon <werggf@naver.com>
2024-09-20 13:39:13 +0900

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {BrowserRouter} from "react-router-dom";
+
+
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
